c3dea01e1bc593b04a7933201b66054182309536
Joshua Dwire <joshua@joshuadwire.com>
2016-09-19 10:49:04 -0400

    Add logout button to PostLogin screen

--- src/controller/PostLoginController.java
+++ src/controller/PostLoginController.java
@@ -1,7 +1,18 @@
 package controller;
 
+import javafx.fxml.FXML;
+
 /**
  * Created by Ashwin on 9/17/2016.
  */
 public class PostLoginController {
+
+    /**
+     * Close menu item event handler
+     */
+    @FXML
+    private void handleLogOut() {
+
+
+    }
 }
