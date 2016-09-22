54f2cf9013086072fd549fbe149927407bf73d8c
Jack Weinkselbaum <jweinkselbaum@gmail.com>
2016-09-22 11:55:20 -0400

    Edited LoginController with new method

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.User;
 
 
@@ -19,18 +20,35 @@ public class LoginController {
 
     private User user;
 
-    private boolean _okClicked = false;
+    private Stage _dialogStage;
 
-    private void handleOKPressed() {
+    private boolean _loginAllowed = false;
+
+    private void handleLoginPressed() {
         if (isInputValid()) {
             if (usernameField.getAccessibleText().equals(user.getUsername())
                     && passwordField.getAccessibleText().equals(user.getPassword())) {
-                _okClicked = true;
+                _loginAllowed = true;
+                _dialogStage.close();
+            } else {
+                _loginAllowed = false;
             }
+
         }
     }
 
     private boolean isInputValid() {
-        return false;
+        String error = "";
+        if (usernameField.getText() == null) {
+            error += "No username entered. Try again!\n";
+        } else if (passwordField.getText() == null) {
+            error+= "No password entered. Try again!\n";
+        }
+
+        if (error.length() == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
