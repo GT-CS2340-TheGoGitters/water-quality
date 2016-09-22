b5d2a71c96d42ddb615fe227050b865208034f3b
Allison Rapoport <arpoport3@gatech.edu>
2016-09-22 11:55:40 -0400

    Edited welcome controller; changed hello world to our name; edited welcome.fxml

--- src/controller/WelcomeController.java
+++ src/controller/WelcomeController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class WelcomeController {
+
+    @FXML
+    private Button login;
+
+    private boolean _loginClicked = false;
+
+    public final void setOnAction(EventHandler<ActionEvent> value) {
+        _loginClicked = true;
+    }
+
+    @FXML
+    private void handleLoginClick() {
+        if (_loginClicked) {
+            //TODO: make it switch screens
+        }
+    }
+
 }
