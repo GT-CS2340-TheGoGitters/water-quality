ba38886d50bc1560fc59e91eadf1121f93051833
Joshua Dwire <joshua@joshuadwire.com>
2016-09-29 11:48:07 -0400

    Add email, title, and home address to Account object

--- src/model/Account.java
+++ src/model/Account.java
@@ -7,6 +7,10 @@ public class Account {
     private String username;
     private String password;
     private String name;
+    private String emailAddress = null;
+    private String title = null;
+    private String homeAddress = null;
+
     private AccountType type;
 
     public Account(String name, String username, String password, AccountType type) {
@@ -34,6 +38,30 @@ public class Account {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
     }
 
     public void setAccountType(AccountType type) {
