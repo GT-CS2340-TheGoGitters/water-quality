5a70474ad3574827cdc39adea7a726bd096a4538
Ashwin Ignatius <aignatius3@gatech.edu>
2016-11-05 19:09:58 -0400

    Restoring AdminHome and AdminHomeController

--- src/controller/AdminHomeController.java
+++ src/controller/AdminHomeController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import model.*;
 
+import java.io.File;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.LinkedList;
@@ -23,32 +24,11 @@ import java.util.ResourceBundle;
 /**
  * Created by Allison on 10/20/16.
  */
-public class AdminHomeController implements Initializable, MapComponentInitializedListener{
+public class AdminHomeController implements Controller {
 
     private WaterQualityApplication mainApp;
 
     private Account account;
-
-    private GoogleMap map;
-
-    @FXML
-    private GoogleMapView mapView;
-
-    @FXML
-    private MenuItem mngUsers;
-
-    @FXML
-    private MenuItem viewLog;
-
-    @FXML
-    private MenuItem mngReports;
-
-    @FXML
-    private MenuItem logout;
-
-    private boolean mapInitialized = false;
-
-    private Queue<WaterReport> pendingReports = new LinkedList<>();
 
     public AdminHomeController() {
 
@@ -60,104 +40,8 @@ public class AdminHomeController implements Initializable, MapComponentInitializ
      */
     public void setApp(WaterQualityApplication newApp) {
         mainApp = newApp;
-
-        for (WaterReport report : WaterReportsHolder.getValues()) {
-            addWaterReport(report);
-        }
     }
 
-    @FXML
-    public void initialize(URL url, ResourceBundle rb) {
-        mapView.addMapInializedListener(this);
-    }
-
-    /**
-     * Initialize the map for the AdminHome page
-     */
-    public void mapInitialized() {
-        mapInitialized = true;
-
-        //Set the initial properties of the map.
-        MapOptions mapOptions = new MapOptions();
-
-        mapOptions.center(new LatLong(39.8282, -98.5795))
-                .mapType(MapTypeIdEnum.ROADMAP)
-                .overviewMapControl(false)
-                .panControl(false)
-                .rotateControl(false)
-                .scaleControl(false)
-                .streetViewControl(false)
-                .zoomControl(false)
-                .zoom(4);
-
-        map = mapView.createMap(mapOptions);
-
-        while (pendingReports.size() != 0) {
-            addWaterReport(pendingReports.remove());
-        }
-    }
-
-    /**
-     * Puts WaterReport on map
-     * @param report WaterReport to be added to map
-     */
-    private void addWaterReport(WaterReport report) {
-        if (!mapInitialized) {
-            pendingReports.add(report);
-            return;
-        }
-
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(report.getLocation().toLatLong());
-
-        // Icons from http://kml4earth.appspot.com/icons.html
-        String infoWindowContent = "";
-        if (report instanceof WaterSourceReport) {
-            WaterSourceReport sourceReport = (WaterSourceReport) report;
-
-            // Set Icon
-            markerOptions.icon("http://maps.google.com/mapfiles/kml/pal5/icon11.png");
-
-            // Set Info Window Text
-            infoWindowContent += "<h2>Water Source Report</h2>";
-            infoWindowContent += "<div><strong>Account:</strong> " + report.getCreator() + "</div>";
-            infoWindowContent += "<div><strong>Date:</strong> " + new SimpleDateFormat("mm/dd/yyy HH:mm").format(report.getCreated()) + "</div>";
-            infoWindowContent += "<div><strong>Type:</strong> " + sourceReport.getWaterType() + "</div>";
-            infoWindowContent += "<div><strong>Overall Condition:</strong> " + sourceReport.getWaterOverallCondition() + "</div>";
-        } else if (report instanceof WaterPurityReport) {
-            WaterPurityReport purityReport = (WaterPurityReport) report;
-            WaterCondition condition = purityReport.getWaterCondition();
-
-            if (condition == WaterCondition.SAFE) {
-                markerOptions.icon("http://maps.google.com/mapfiles/kml/pal3/icon53.png");
-            } else if (condition == WaterCondition.TREATABLE) {
-                markerOptions.icon("http://maps.google.com/mapfiles/kml/pal3/icon49.png");
-            } else if (condition == WaterCondition.UNSAFE) {
-                markerOptions.icon("http://maps.google.com/mapfiles/kml/pal3/icon51.png");
-            }
-
-            // Set Info Window Text
-            infoWindowContent += "<h2>Water Purity Report</h2>";
-            infoWindowContent += "<div><strong>Account:</strong> " + report.getCreator() + "</div>";
-            infoWindowContent += "<div><strong>Date:</strong> " + new SimpleDateFormat("mm/dd/yyy HH:mm").format(report.getCreated()) + "</div>";
-            infoWindowContent += "<div><strong>Condition:</strong> " + condition + "</div>";
-            infoWindowContent += "<div><strong>Virus:</strong> " + purityReport.getVirusPPM() + " PPM</div>";
-            infoWindowContent += "<div><strong>Contaminant:</strong> " + purityReport.getContaminantPPM() + " PPM</div>";
-        }
-
-        Marker marker = new Marker(markerOptions);
-
-        map.addMarker(marker);
-
-        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
-        infoWindowOptions.content(infoWindowContent);
-
-        InfoWindow infoWindow = new InfoWindow(infoWindowOptions);
-
-        map.addUIEventHandler(marker, UIEventType.click, (netscape.javascript.JSObject obj) -> {
-            infoWindow.open(map, marker);
-        });
-    }
 
 
 
@@ -178,7 +62,7 @@ public class AdminHomeController implements Initializable, MapComponentInitializ
 
     @FXML
     public void handleLogout() {
-        mainApp.returnToWelcomeScreen();
+        mainApp.showSceen(new File("../view/Welcome.fxml"));
     }
 
 }
